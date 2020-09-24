aaa54c3e8f4a84e10f90dc638c37b3241704124c
Romain Laurent <romain.laurent@cdiscount.com>
2020-09-24 17:39:08 +0200

    feat: add static Header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,20 +1,43 @@
 import React from "react";
 import { string } from "prop-types";
+import {
+  Avatar,
+  AppBar,
+  Badge,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
 
 import yamotoLogo from "./yamoto-logo.jpg";
-import { Badge, Wrapper, Logo, Title } from "./styles";
+import useStyles from "./styles";
 
 const websiteTitle = "Yamoto Sushi";
 
 export default function Header({ title }) {
+  const classes = useStyles();
+
   return (
-    <Wrapper>
-      <Logo src={yamotoLogo} alt={websiteTitle} />
-      <Title>
-        {websiteTitle} - {title}
-      </Title>
-      <Badge>3</Badge>
-    </Wrapper>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Avatar
+            alt={websiteTitle}
+            src={yamotoLogo}
+            className={classes.logo}
+          />
+          <Typography variant="h6" className={classes.title}>
+            {websiteTitle} - {title}
+          </Typography>
+          <IconButton aria-label="show 17 new notifications" color="inherit">
+            <Badge badgeContent={17} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
--- src/Header/styles.js
+++ src/Header/styles.js
@@ -1,33 +1,13 @@
-import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
 
-export const Logo = styled.img`
-  width: 140px;
-  height: 140px;
-`;
-
-const headerHeight = 140;
-
-export const Title = styled.h1`
-  display: inline-block;
-  margin: 50px 0 0 20px;
-  vertical-align: top;
-`;
-
-export const Wrapper = styled.header`
-  height: ${headerHeight}px;
-  border-bottom: 1px solid grey;
-`;
-
-export const Badge = styled.span`
-  position: absolute;
-  right: 20px;
-  top: 40px;
-  border-radius: 50%;
-  width: 40px;
-  height: 40px;
-  background-color: #eb5926;
-  text-align: center;
-  font-weight: bold;
-  font-size: 20px;
-  color: white;
-`;
+export default makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  logo: {
+    marginRight: theme.spacing(2),
+  },
+}));
